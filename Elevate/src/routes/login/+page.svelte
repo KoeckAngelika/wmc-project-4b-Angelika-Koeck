cc974ed1ffe0b81f8ad686b07a2fda9ee1f6edce
KoeckAngelika <angelika.lena.koeck@gmail.com>
2026-05-15 14:07:49 +0200

    Login implementieren

---
+++ Elevate/src/routes/login/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+
+</script>
+
+<div class="page">
+    <div class="card">
+
+        <h1>Willkommen zurück</h1>
+        <p class="subtitle">
+            melde dich an um weiter zu trainieren
+        </p>
+
+        <div class="input-group">
+            <label>Benutzername</label>
+            <input type="text" placeholder="Username">
+        </div>
+        <div class="input-group">
+            <label>Passwort</label>
+            <input type="password" placeholder="*******">
+        </div>
+
+        <p class="forgot">
+            Passwort vergessen?
+        </p>
+
+        <button>
+            Anmelden
+        </button>
+    </div>
+</div>
+
+
